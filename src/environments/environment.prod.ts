91def6a4ef9c15724845ee4533ad17377cf0bbc2
Evert <evert.aov@gmail.com>
2026-09-19 00:23:34 -0400

    fix(env) prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   // Configuración de conexión con el backend en Modo Producción (PROD)
-  apiUrl: 'http://localhost:3000/api',
-  socketUrl: 'http://localhost:3000/collaboration',
+  apiUrl: 'https://api.evertrodriguez.dev/api',
+  socketUrl: 'https://api.evertrodriguez.dev/collaboration',
 };
